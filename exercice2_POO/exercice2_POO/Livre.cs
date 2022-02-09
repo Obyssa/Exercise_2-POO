be22be96fc9126f0c41c0eb410f3516a24a39b5e
LAPENA Matheo <matheo.lapena@gmail.com>
2022-02-09 09:25:25 +0100

    Exercice 2 fini

---
+++ exercice2_POO/exercice2_POO/Livre.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classe_exo2
+{
+    class Livre
+    {
+
+
+        private string titre;
+        private string auteur;
+        private int prix;
+
+        public Livre(string titre, string auteur, int prix)
+        {
+
+            this.titre = titre;
+            this.auteur = auteur;
+            this.prix = prix;
+
+        }
+
+
+        public string Titre
+        {
+            get
+            {
+                return titre;
+            }
+
+            set
+            {
+                titre = value;
+            }
+
+        }
+
+        public string Auteur
+        {
+
+            get
+            {
+
+                return auteur;
+
+            }
+            set
+            {
+                auteur = value;
+            }
+
+        }
+
+        public int Prix
+        {
+            get
+            {
+                return prix;
+            }
+            set
+            {
+                prix = value;
+            }
+        }
+
+        public string Afficher()
+        {
+            return Titre + " " + Auteur + " " + Prix;
+        }
+
+
+    }
+}
--- exercice2_POO/exercice2_POO/Program.cs
+++ exercice2_POO/exercice2_POO/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace exercice2_POO
+namespace classe_exo2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Livre livre = new Livre("mais nan", "bah oe", 12);
+
+            Livre livre1 = new Livre("c'est ça", "moi", 12);
+
+            Console.WriteLine(livre.Afficher());
+            Console.WriteLine(livre1.Afficher());
+
         }
     }
 }
